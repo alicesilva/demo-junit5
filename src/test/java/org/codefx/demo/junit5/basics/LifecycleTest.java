e077cfb6bbf82c0481e6dbd2f6f41dd5114c4ff6
alicesilva <alice.silva@ccc.ufcg.edu.br>
2018-10-17 14:38:30 -0300

    first commit

---
+++ src/test/java/org/codefx/demo/junit5/basics/LifecycleTest.java
@@ -0,0 +1,65 @@
+package org.codefx.demo.junit5.basics;
+
+import org.codefx.demo.junit5.DisabledOnOs;
+import org.codefx.demo.junit5.OS;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+class LifecycleTest {
+
+	@BeforeAll
+	static void initializeExternalResources() {
+		System.out.println("Initializing external resources...");
+	}
+
+	@BeforeEach
+	void initializeMockObjects() {
+		System.out.println("Initializing mock objects...");
+	}
+
+	@Test
+	void someTest() {
+		System.out.println("Running some test...");
+		assertTrue(true);
+	}
+
+	@Test
+	void otherTest() {
+		assumeTrue(true);
+
+		System.out.println("Running another test...");
+		assertNotEquals(1, 42, "Why would these be the same?");
+	}
+
+	@Test
+	@Disabled
+	void disabledTest() {
+		System.exit(1);
+	}
+
+	@Test
+	@DisabledOnOs(OS.NIX)
+	void disabledNixTest() {
+		fail("Only runs on Unix/Linux");
+	}
+
+	@AfterEach
+	void tearDown() {
+		System.out.println("Tearing down...");
+	}
+
+	@AfterAll
+	static void freeExternalResources() {
+		System.out.println("Freeing external resources...");
+	}
+
+}
